2a82cfa89d0637793dc759c5b3f5c4bb7a3f2e64
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-09-21 12:49:31 -0400

    finalized app , README missing

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'root',
+        database: 'employee_db'
+    },
+    
+    console.log('\u001b[33m', `Connected to the employee_db database ✔`)
+); 
+
+module.exports = db;
